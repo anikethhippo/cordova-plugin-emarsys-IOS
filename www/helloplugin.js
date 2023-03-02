90a844beee64a4522fd3c8bef1dff78cbc21979b
anikethhippo <aniketh.keshava@hippoinnovations.com>
2023-03-02 12:03:50 +0530

    Emarsys Plugin

---
+++ www/helloplugin.js
@@ -0,0 +1,7 @@
+var helloplugin = {
+    
+    sayHello: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback,errorCallback,'HelloPlugin','sayHello',[{"name" :name}]);
+    }
+}
+module.exports = helloplugin;
