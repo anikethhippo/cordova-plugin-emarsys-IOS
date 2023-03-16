7e9ebb4605f5bfb82bd8e8965683e6486d2814b4
anikethhippo <aniketh.keshava@hippoinnovations.com>
2023-03-16 18:56:15 +0530

    Emarsys track added

--- www/emarsys.js
+++ www/emarsys.js
@@ -1,6 +1,6 @@
 var emarsys = {
     
-    setup: function (args, successCallback, errorCallback) {
+    track: function (args, successCallback, errorCallback) {
         cordova.exec(successCallback,errorCallback,'EmarsysPlugin','track',args);
     }
 }
