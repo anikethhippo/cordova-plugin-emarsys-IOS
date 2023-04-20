4abf35b074a90617ab8a65f0e730b94060f36c9a
anikethhippo <aniketh.keshava@hippoinnovations.com>
2023-04-20 14:12:53 +0530

    Emarsys R.string constant

--- src/android/EmarsysApplication.java
+++ src/android/EmarsysApplication.java
@@ -3,7 +3,6 @@ import com.emarsys.config.EmarsysConfig;
 import com.emarsys.Emarsys;
 import android.app.Application;
 
-
 import android.app.NotificationManager;
 import android.app.NotificationChannel;
 import android.R;
@@ -26,10 +25,10 @@ private void createNotificationChannel() {
   // Create the NotificationChannel, but only on API 26+ because
   // the NotificationChannel class is new and not in the support library
 	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-    CharSequence name = getString(R.string.channel_name);
-    String description = getString(R.string.channel_description);
+    //CharSequence name = getString(R.string.channel_name);
+    //String description = getString(R.string.channel_description);
     int importance = NotificationManager.IMPORTANCE_DEFAULT;
-    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+    NotificationChannel channel = new NotificationChannel("xyz123", "Emarsys-test", importance);
     channel.setDescription(description);
     // Register the channel with the system; you can't change the importance
     // or other notification behaviors after this
