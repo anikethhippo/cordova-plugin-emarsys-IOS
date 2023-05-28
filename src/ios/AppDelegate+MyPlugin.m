a620a658c7f29c61265eb29d77d5cb6ead50077d
anikethhippo <aniketh.keshava@hippoinnovations.com>
2023-05-29 03:43:02 +0530

    Emarsys IOS

--- src/ios/AppDelegate+MyPlugin.m
+++ src/ios/AppDelegate+MyPlugin.m
@@ -1,11 +1,11 @@
 #import "AppDelegate.h"
-
+#import "AppDelegate+MyPlugin.h"
 @implementation AppDelegate (MyPlugin)
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     EMSConfig *config = [EMSConfig makeWithBuilder:^(EMSConfigBuilder *builder) {
-        [builder setMobileEngageApplicationCode:<applicationCode: NSString>];
-        [builder setMerchantId:<merchantId: NSString>];
+        [builder setMobileEngageApplicationCode:"EMS20-DFDF4"];
+        [builder setMerchantId:"1CEA5AF1FE153881"];
         [builder enableConsoleLogLevels:@[<EMSLogLevel.trace, EMSLogLevel.debug, EMSLogLevel.info, EMSLogLevel.warn, EMSLogLevel.error, EMSLogLevel.basic>]];
     }];
     [Emarsys setupWithConfig:config];
