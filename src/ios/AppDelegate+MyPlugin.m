bea99a6d855f372e94c07485f355558c80c0f1e4
anikethhippo <aniketh.keshava@hippoinnovations.com>
2023-06-14 18:58:11 +0530

    fix log issue

--- src/ios/AppDelegate+MyPlugin.m
+++ src/ios/AppDelegate+MyPlugin.m
@@ -8,9 +8,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     EMSConfig *config = [EMSConfig makeWithBuilder:^(EMSConfigBuilder *builder) {
-        [builder setMobileEngageApplicationCode:"EMS20-DFDF4"];
-        [builder setMerchantId:"1CEA5AF1FE153881"];
-        [builder enableConsoleLogLevels:@[<EMSLogLevel.trace, EMSLogLevel.debug, EMSLogLevel.info, EMSLogLevel.warn, EMSLogLevel.error, EMSLogLevel.basic>]];
+        [builder setMobileEngageApplicationCode:"@EMS20-DFDF4"];
+        [builder setMerchantId:"@1CEA5AF1FE153881"];
+        //[builder enableConsoleLogLevels:@[<EMSLogLevel.trace, EMSLogLevel.debug, EMSLogLevel.info, EMSLogLevel.warn, EMSLogLevel.error, EMSLogLevel.basic>]];
     }];
     [Emarsys setupWithConfig:config];
 	 // Handle push 
